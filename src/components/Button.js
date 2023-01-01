f46de0771f30bcf5aa3713e25c856ca48b4bec28
Милош Е. Ђурђевић <milos.om.dj@gmail.com>
2023-01-01 23:26:27 +0100

    New Year's Revision #1

--- src/components/Button.js
+++
@@ -1,7 +0,0 @@
-import '../assets/stylesheets/Button.css';
-
-const Button = (props) => {
-    return <button className="btn" onClick={props.handleClick}>{props.title}</button>
-};
-
-export default Button;
