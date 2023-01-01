f5303d5660541b1c204471a0c44587de225fbc4d
Милош Е. Ђурђевић <milos.om.dj@gmail.com>
2023-01-01 23:52:16 +0100

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import Header from './components/Header';
-import Button from './components/Button';
-import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
-
-  const resetCount = () => {
-    setCount(0);
-  };
-
+const App = () => {
   return (
-    <div className="container main">
-      <Header />
-      <div className="container actions">
-        <span className="counter">{count}</span>
-        <Button title="+" handleClick={incrementCount} />
-        <Button title="Reset" handleClick={resetCount} />
-      </div>
-    </div>
+    <Header />
   );
-}
+};
 
 export default App;
