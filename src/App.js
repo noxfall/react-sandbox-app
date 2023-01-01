52d72ad32e60571e19effb638ff5e159d5352420
Милош Е. Ђурђевић <milos.om.dj@gmail.com>
2023-01-02 00:17:32 +0100

    Finish React Counter App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Button from './components/Button';
 import Header from './components/Header';
+import { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    return setCount(count + 1);
+  };
+
+  const resetCount = () => {
+    return setCount(0);
+  }
+
   return (
     <div className="container main">
       <Header />
       <div className="container actions">
-        <span className="counter">0</span>
-        <Button title="+" />
-        <Button title="reset" />
+        <span className="counter">{count}</span>
+        <Button title="+" handleClick={incrementCount} />
+        <Button title="reset" handleClick={resetCount} />
       </div>
     </div>
   );
